7510c8dc41b255d98f2ede8224aa46cffcdd536b
joykara <joykarani1@gmail.com>
2023-05-24 22:37:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const tableBody = document.getElementById('table-body');
+
+fetch('generated[223].json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        data.forEach(function (data) {
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${data.name}</td>
+                                <td>${data.age}</td>
+                                <td>${data.gender}</td>
+                                <td>${data.email}</td>
+                                <td>${data.phone}</td>
+                                <td>${data.address}</td>`;
+            tableBody.appendChild(row);
+        });
+    }
+    );
